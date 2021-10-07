73a32a11f367e058b48748e8d5cd57594577a0b5
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-10-07 14:15:43 +0600

    Mehedi

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
-class loginform(forms.Form):
+class Loginform(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth import authenticate,login
+from .forms import Loginform
 
 # Create your views here.
